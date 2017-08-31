5f03a71c5ea9856f28cf75cb5451fa983a633c0b
shieldax <Shieldax@gmail.com>
2017-08-31 19:13:47 +0800

    delete unused themes

--- wp-content/themes/xt-corporate-lite/templates/content-page.php
+++
@@ -1,5 +0,0 @@
-<div class="featured-image">
-    <?php the_post_thumbnail(); ?>
-</div>
-<?php the_content(); ?>
-<?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'xt-corporate-lite'), 'after' => '</p></nav>']); ?>
--- wp-content/themes/xt-corporate-lite/templates/page-header.php
+++
@@ -1,4 +0,0 @@
-<div class="page-header">
-  <h1><?php echo xt_corporate_lite_title(); ?></h1>
-</div>
-
